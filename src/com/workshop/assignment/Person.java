6d1df6bdc1f9fa1137fc0d3195522e488d24a14c
Subhashree naik <subhashree.naik@gmail.com>
2022-04-01 08:46:20 +0530

    Adding information to person object

--- src/com/workshop/assignment/Person.java
+++ src/com/workshop/assignment/Person.java
@@ -13,6 +13,74 @@ public class Person {
 	private String phoneNo;
 	private String email;
 	
+	
+	/** getter and setter methods **/
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 	/*
 	 * Declaring parameterised constructor of class
 	 */
@@ -26,5 +94,11 @@ public class Person {
 		this.phoneNo=phoneNo;
 		this.email = email;
 		
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNo=" + phoneNo + ", email=" + email + "]";
 	}
 }
